a0f07afc0214b5e12d66d394003854aa8b095929
sufeeeringGavinM <159094807+sufeeeringGavinM@users.noreply.github.com>
2026-03-08 20:49:29 +0800

    Update downloader.bat
    downloads video to same directory

--- downloader.bat
+++ downloader.bat
@@ -1,19 +1,25 @@
 @echo off
 
+:: takes clipboard content as variable
 for /f "eol=; tokens=*" %%I in ('powershell Get-Clipboard') do set id=%%I
 
-yt-dlp.exe --recode mp4 -o "G:/DOWNLOAD/%%(id)s.%%(ext)s" %id%
+:: uncomment this if you don't want autoupdate everytime
+:: G:\DOWNLOAD\yt-dlp.exe -U
 
-yt-dlp.exe --skip-download --print id %id% > temporarilementedesicration.txt
+:: actual downloading, remove --recode mp4 if you want original format
+.\yt-dlp.exe --recode mp4 -o "%%(id)s.%%(ext)s" %id%
+
+:: why id? unique parse-friendly names!
+.\yt-dlp.exe --skip-download --print id %id% > temporarilementedesicration.txt
 
 set /p my_variable=<temporarilementedesicration.txt
 
-set TARGETFILEPATH=G:\DOWNLOAD\%my_variable%.mp4
+set TARGETFILEPATH=%my_variable%.mp4
 
 @echo on
 
-fcopy.bat %TARGETFILEPATH%
+.\fcopy.bat %TARGETFILEPATH%
 
 del temporarilementedesicration.txt
 
-exit
+
